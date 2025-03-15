addff9f8dd68a93f12105e3b1ca4d78af4a2762f
AndreRibeiroESMAD <40240475@esmad.ipp.pt>
2025-03-15 15:31:41 +0000

    ficha3.2

---
+++ js.js
@@ -0,0 +1,19 @@
+let btn = document.getElementById("sub")
+
+btn.addEventListener("click", function(){
+    event.preventDefault();
+
+    let name = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let num = document.getElementById("num").value
+    let morada = document.getElementById("morada").value
+    let nif = document.getElementById("nif").value
+
+    if (name && email && num && morada && nif){
+        console.log(name, email, num, morada, nif)
+    }
+    else{
+        alert("Porfavor preencha todos os campos")
+    }
+    
+})
